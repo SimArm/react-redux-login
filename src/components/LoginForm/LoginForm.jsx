86619bb52b7ceb1103684af91337962bb374a1fb
Simas Armolaitis <s.armolaitis@gmail.com>
2021-04-06 19:37:40 +0300

    Login with validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import './LoginForm.scss';
 
 const LoginForm = () => {
+    const user = useSelector(state => state.Users);
+    const isLogged = useSelector(state => state.isLogged);
+
+    const [nameValue, setName] = useState('');
+    const [passwordValue, setPassword] = useState('');
+    const [isError, setError] = useState(false);
+
+    const errorClasses = isError ? 'errorMessage' : 'hidden';
+
+    const history = useHistory();
+
+    const updateName = (event) => {
+        const { value } = event.target;
+        setName(value);
+    }
+
+    const updatePassword = (event) => {
+        const { value } = event.target;
+        setPassword(value);
+    }
+
+    const onLogin = () => {
+        nameValue === user[0].name && passwordValue === user[0].password ?
+        history.push('./home')
+        : setError(true);
+    }
+
     return (
         <div clasName='loginWrapper'>
             <h1>Login</h1>
+            <p className={errorClasses}>Name and password do not match</p>
             <form>
                 <label htmlFor='Name'>Name</label>
-                <input type='text' id='Name' placeholder='Name'></input>
+                <input type='text' id='Name' placeholder='Name' value={nameValue} onChange={updateName}></input>
                 <label htmlFor='Password'>Password</label>
-                <input type='password' id='Password' placeholder='Password'/>
+                <input type='password' id='Password' placeholder='Password' value={passwordValue} onChange={updatePassword}/>
             </form>
-            <button>Next</button>
+            <button onClick={onLogin}>Next</button>
         </div>
     );
 }
