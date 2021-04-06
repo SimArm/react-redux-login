f0a26b3de144e9fe710cca8c781862eb124be43e
Simas Armolaitis <s.armolaitis@gmail.com>
2021-04-06 18:19:14 +0300

    StartingStructure

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginForm.scss';
+
+const LoginForm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default LoginForm;
---
+++ src/components/MainPanel/MainPanel.jsx
@@ -0,0 +1,12 @@
+import React from './react';
+import './MainPanel.scss';
+
+const MainPanel = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default MainPanel;
